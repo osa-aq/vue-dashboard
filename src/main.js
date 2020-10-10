f3f23df16fd9c67a5f79953e7a0ec716800d654e
Osama Abu Quboh <osa.aqu@gmail.com>
2020-10-10 16:13:49 +0300

    Convert static data to APIs

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
-import {makeServer} from '../mock/server'
+import {makeServer} from './mock/server'
 
 // Style
 import 'normalize.css/normalize.css'
